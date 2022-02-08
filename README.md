059adefa902540741214095be92a71d41b7fb81c
Levi M. Williamson <levimwill@gmail.com>
2022-02-07 17:16:06 -0800

    initial commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# README
+
+This project requires [Node.js](https://nodejs.org/en/).
+Installation instructions are provided in the given link.
+
+## Setup
+Navigate to the project directory.
+```sh
+cd assignment1
+```
+Install dependencies.
+```
+npm i
+```
+
+## Run
+Set required environment variables:
+```sh
+export PORT=8080
+export WEBSOCKET_PORT=5000
+export SERIAL_PORT=9600
+export SERIAL_FILE='/dev/ttyACM0'
+```
+Start the server.
+```sh
+npm start
+```
---
+++ app.mjs
@@ -0,0 +1,32 @@
+import assert from 'assert/strict'
+import WebSocket from 'ws'
+import SerialPort from 'serialport'
+import Readline from '@serialport/parser-readline'
+import express from 'express'
+
+// check for required environment variables
+assert.notStrictEqual(process.env.SERIAL_FILE, undefined, 'Must provide SERIAL_FILE environment variable.')
+assert.notStrictEqual(process.env.SERIAL_PORT, undefined, 'Must provide SERIAL_PORT environment variable.')
+assert.notStrictEqual(process.env.WEBSOCKET_PORT, undefined, 'Must provide WEB_SOCKET_PORT environment variable.')
+assert.notStrictEqual(process.env.PORT, undefined, 'Must provide PORT environment variable.')
+
+let sockets = []
+const server = new WebSocket.Server({ port: process.env.WEBSOCKET_PORT })
+server.on('connection', socket => {
+	sockets.push(socket)
+	socket.on('close', () => {
+		sockets = sockets.filter( s => s !== socket )
+	})
+})
+
+const serialport = new SerialPort(process.env.SERIAL_FILE, { baudRate: Number(process.env.SERIAL_PORT) })
+const parser = serialport.pipe(new Readline({ delimiter: '\n' }))
+parser.on('data', data => {
+	sockets.forEach( socket => socket.send(data) )
+})
+
+const app = express()
+app.use(express.static('./static/'))
+app.listen(process.env.PORT, () => console.log(`HTTP server listening on port ${process.env.PORT}
+Web Socket server listening on port ${process.env.WEBSOCKET_PORT}`))
+
